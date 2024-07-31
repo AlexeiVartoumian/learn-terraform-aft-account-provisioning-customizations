846f3b10df21f67886f8baaf830bd145e2545fb2
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-07-31 17:01:06 +0100

    try again

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,6 +2,38 @@ import {
   to = aws_iam_role.aft_states
   id = "aft-account-provisioning-customizations-role"
 }
+
+
+
+
+
+
+
+
+
+# resource "aws_iam_role" "aft_states" {
+#   name               = "aft-account-provisioning-customizations-role"
+#   assume_role_policy = templatefile("${path.module}/iam/trust-policies/states.tpl", { none = "none" })
+# }
+resource "aws_iam_role" "aft_states" {
+  name               = "aft-account-provisioning-customizations-role"
+  assume_role_policy = templatefile("${path.module}/iam/trust-policies/states.tpl", {
+    alternate_contacts_customizations_sfn_arn = module.alternate-contacts.aft_alternate_contacts_state_machine_arn
+  })
+}
+
+resource "aws_iam_role_policy" "aft_states" {
+  name = "aft-account-provisioning-customizations-policy"
+  role = aws_iam_role.aft_states.id
+
+  policy = templatefile("${path.module}/iam/role-policies/iam-aft-states.tpl", {
+    data_aws_region                             = data.aws_region.aft_management_region.name
+    data_aws_account_id                         = data.aws_caller_identity.aft_management_id.account_id
+    alternate_contacts_customizations_sfn_arn   = module.alternate-contacts.aft_alternate_contacts_state_machine_arn
+  })
+}
+
+
 # resource "aws_iam_role" "aft_states" {
 #   name               = "aft-account-provisioning-customizations-role"
 #   assume_role_policy = templatefile("${path.module}/iam/trust-policies/states.tpl", { none = "none" })
@@ -15,19 +47,3 @@ import {
 #     account_provisioning_customizations_sfn_arn = aws_sfn_state_machine.aft_account_provisioning_customizations.arn
 #   })
 # }
-
-resource "aws_iam_role" "aft_states" {
-  name               = "aft-account-provisioning-customizations-role"
-  assume_role_policy = templatefile("${path.module}/iam/trust-policies/states.tpl", { none = "none" })
-}
-
-resource "aws_iam_role_policy" "aft_states" {
-  name = "aft-account-provisioning-customizations-policy"
-  role = aws_iam_role.aft_states.id
-
-  policy = templatefile("${path.module}/iam/role-policies/iam-aft-states.tpl", {
-    data_aws_region                             = data.aws_region.aft_management_region.name
-    data_aws_account_id                         = data.aws_caller_identity.aft_management_id.account_id
-    alternate_contacts_customizations_sfn_arn   = module.alternate-contacts.aft_alternate_contacts_state_machine_arn
-  })
-}
